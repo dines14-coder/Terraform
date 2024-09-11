d6b24d6081b4ef8fede200a52f98aee4df16f638
dines14-coder <150772284+dines14-coder@users.noreply.github.com>
2024-09-11 17:54:49 +0530

    with points

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,5 +1,5 @@
-variable Aws_accessKey {}
-variable Aws_secretKey {}
+variable Aws_accessKey {}                           #we should save the file as terraform.tfvars which keeps the variable and the value
+variable Aws_secretKey {}                          # $ dollar symbol is used for the reference. if we use that means the following will be used here  
 variable Aws_region {
     default = "us-east-2"
 }
@@ -17,6 +17,6 @@ variable Amis {
 
 variable aws_sg {
   type        = list
-  default     = ["sg_001", "sg_002", "sg_003" ]
+  default     = ["sg_001", "sg_002", "sg_003" ]         
 }
 
