095fbe0d521890f56751d9ea085af0113434fcc6
dines14-coder <150772284+dines14-coder@users.noreply.github.com>
2024-09-11 17:19:02 +0530

    new

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "ec2" {
-  ami                     = "ami-0dcc1e21636832c5d"
+  ami                     = lookup(var.amis, var.Aws_region)
   instance_type           = "t2.micro"
 }
--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = lookup(var.amis, var.Aws_region)
+  region     = "${var.Aws_region}"
   access_key = "${var.Aws_accessKey}"
   secret_key = "${var.Aws_secretKey}"
 }
